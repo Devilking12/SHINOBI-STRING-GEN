cf6d10a5022a0af5cb92b04ed41404499f2a2cb7
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2025-08-14 13:18:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = getenv("API_HASH", "4c246fb0c649477cc2e79b6a178ddfaa")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 OWNER_ID = int(getenv("OWNER_ID", "8447127606"))
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://SHASHANK:STRANGER@shashank.uj7lold.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://heistteam1:heistteam1@cluster0.24oegbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 MUST_JOIN = getenv("MUST_JOIN", "ITSZSHUKLA")
 START_IMG = getenv("START_IMG", "https://files.catbox.moe/520y6h.jpg")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "MASTIWITHFRIENDSXD")
