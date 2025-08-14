8041ef05687d6122b7511911bde71dcc753cdb8b
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2025-08-14 13:33:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "27613077"))
 API_HASH = getenv("API_HASH", "19dccde5b307a489ec9ded3a8a558e34")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "8235842848:AAHWduszLN8fTgmKAYGO9xFBx2MchguLmq8")
 
 OWNER_ID = int(getenv("OWNER_ID", "8447127606"))
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://heistteam1:heistteam1@cluster0.24oegbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
